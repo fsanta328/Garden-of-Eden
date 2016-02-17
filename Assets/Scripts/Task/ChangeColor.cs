44779628956198ff0920a4911e9fc7b9cc4b549d
fsanta328 <sharaf@Goo.local>
2016-02-17 14:58:45 +0100

    Initial commit
    Commit contains
    -Some AI behaviour
    -Basic map layout

---
+++ Assets/Scripts/Task/ChangeColor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviourTree;
+
+public class ChangeColor : ITaskDefinition 
+{
+	Color currentColor;
+
+	public ChangeColor(Color a_color)
+	{
+		currentColor = a_color;
+	}
+	public void OnEnter()
+	{
+	}
+
+	public void OnExit()
+	{
+	}
+
+	public NodeState OnExecute(BehaviourTrees bTree)
+	{
+		Blackboard m_blackBoard = bTree.GetBlackboard();
+
+		GameObject agent = (GameObject)m_blackBoard.data ["agent"];
+
+		agent.GetComponent<MeshRenderer> ().material.color = currentColor;
+
+		return NodeState.Success;
+	}
+}
