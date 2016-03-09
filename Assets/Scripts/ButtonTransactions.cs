07dfbaf62058df4ef61389958623e94028d1bec1
Anas bitar <a.bitar@saedubai.com>
2016-03-09 21:17:25 +0400

    Light, skybox, portal (created my own particle system download online textures) pls merge menus

---
+++ Assets/Scripts/ButtonTransactions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonTransactions : MonoBehaviour {
+
+	// Use this for initialization
+	public void ChangeScene (string sceneChange) 
+	{
+		Application.LoadLevel(sceneChange);
+	
+	}
+	
+	public void TogglePanel (GameObject panel) 
+	{
+		panel.SetActive (!panel.activeSelf);
+	}
+}
